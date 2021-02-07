170b99380563c390b93e1bf05d8a4f9341e2886e
Thai Truong <sontruong@hotmail.co.uk>
2021-02-07 23:42:07 +0000

    burger menu added

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const burger = document.querySelector(".burger");
 const burgerMenu = document.querySelector(".burger-icon");
+const navMobile = document.querySelector(".burger-menu");
+
+burger.addEventListener("click", () => {
+  burgerMenu.classList.toggle("animate");
+  navMobile.classList.toggle("active");
+});
