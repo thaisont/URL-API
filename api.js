c680f0de5fb67ad95292651f4ea6681eed89bc95
Tan Ahmed <tannerahmed@gmail.com>
2021-02-18 13:34:55 +0000

    click event - grab search value

--- api.js
+++ api.js
@@ -1,39 +1,45 @@
 // API shortner
+document.addEventListener("DOMContentLoaded", function (event) {
+  const result = document.querySelector(".main-section-result");
+  const shortLink = document.getElementById("short");
+  const link = document.getElementById("link");
+  const copyBtn = document.querySelector(".link-copy");
+  const apiSearchBtn = document.getElementById("api-search-btn");
+  const searchInput = document.getElementById("search-placeholder");
+  console.log(apiSearchBtn, "apiSearchBtn");
 
-const result = document.querySelector(".main-section-result");
-const shortLink = document.getElementById("short");
-const link = document.getElementById("link");
-const copyBtn = document.querySelector(".link-copy");
+  apiSearchBtn.addEventListener("click", (e) => {
+    console.log(searchInput.value, "searchInput");
+    getUrl(searchInput.value);
+  });
 
-const apiUrl =
-  "https://api.shrtco.de/v2/shorten?url=https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G";
+  async function getUrl(urlString) {
+    const response = await fetch(urlString);
+    const data = await response.json();
+    console.log(data);
 
-async function getUrl() {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  console.log(data);
+    const shortUrl = data.result.short_link;
+    const fullUrl = data.result.original_link;
 
-  const shortUrl = data.result.short_link;
-  const fullUrl = data.result.original_link;
+    console.log(shortUrl);
+    console.log(fullUrl);
 
-  console.log(shortUrl);
-  console.log(fullUrl);
+    link.value = fullUrl;
+    shortLink.value = shortUrl;
+  }
+  // getUrl();
 
-  link.value = fullUrl;
-  shortLink.value = shortUrl;
-}
-getUrl();
+  copyBtn.addEventListener("click", (e) => {
+    /* Get the text field */
+    const copyText = document.getElementById("short");
 
-copyBtn.addEventListener("click", (e) => {
-  /* Get the text field */
-  const copyText = document.getElementById("short");
+    /* Select the text field */
+    copyText.select("short");
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-  /* Select the text field */
-  copyText.select("short");
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
 
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  console.log("text copied");
+    console.log("text copied");
+  });
 });
