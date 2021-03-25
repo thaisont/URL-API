25868d5a255b204af20519e52cf2b5fef13aed14
Thai Truong <sontruong@hotmail.co.uk>
2021-03-25 23:32:05 +0000

    padding changes

--- api.js
+++ api.js
@@ -66,3 +66,18 @@ copyBtn.addEventListener("click", (e) => {
 
   console.log("text copied");
 });
+
+const Scroll = () => {
+  if (window) {
+    try {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    } catch (error) {
+      window.scrollTo(0, 0);
+    }
+  }
+};
+
